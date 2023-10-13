b4c28149105b87c9debbb062ccd0a480273ddc41
Niyati Garg <Niyatigarg2002@gmail.com>
2023-10-13 18:36:13 +0530

    created developerProfileContent

--- src/components/DeveloperProfile.js
+++ src/components/DeveloperProfile.js
@@ -1,7 +1,22 @@
 const DeveloperProfile=()=>{
     return(
         <div>
-            
+            <h2> As A Developer</h2>
+            <p>
+                I find immense satisfaction in crafting visually appealiung user interfaces. <br/>
+                My expertise spans a range of technologies including HTML, CSS, javascript, Reac.js and Redux.
+                I've honed my abilities through hands-on experiences and internships.<br/>
+                Crafting user friendly digital experiences, building reusable components and collaborating
+                with talented team have been my evenue of growth.
+
+                In addition to my technical skills, i'vm proficient in modern development tools such as Git and
+                 well-versed popular integrated development Environments (IDEs) like visual studio code and Webstorm.<br/>
+                This efficiency allows me to maximize my productivity, ensuring top-notch results.
+            </p>
+            <div>
+                CAPABLE MINDS OFTEN REMAIN UNSEEN TREASURES, <br/>
+                WAITING FOR THE CHANCE TO SHINE
+            </div>
         </div>
     )
 }
