ead3a6ecd32731e0f6664cd2c433c108098df555
Niyati Garg <Niyatigarg2002@gmail.com>
2023-10-14 17:11:39 +0530

    created a common layout for text in As a developer

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,23 @@
-const About=()=>{
+import './DeveloperProfile.css';
+
+const About = () => {
     return (
-        <div style={{ color: 'white' }}>
-            <h2> About Me</h2>
+        <div className="parent">
+            <h3> - About Me</h3>
+            <h2> Niyati Garg</h2>
             <p>Hello, I'm a passionate front end developer.
-                I'm thrilled to welcome you to my corner of the internet. <br/>
+                I'm thrilled to welcome you to my corner of the internet. <br />
 
-                In 2020, i began my journey into web development during my undergraduate studies. 
+                In 2020, i began my journey into web development during my undergraduate studies.
                 I realized the importance of forging a unique path, considering my diverse academic background.
-                Driven by unwavering commitment, I dove into coding, consistently honing my skills and pushing my boundaries. <br/> <br/>
+                Driven by unwavering commitment, I dove into coding, consistently honing my skills and pushing my boundaries. <br /> <br />
 
-                I'm currently pursuing Masters in Computer Applications from Amity University Online. <br/>
-                Today my mission extends beyond merely securing a job. I aim not just to expand my knowledge 
-                but to excel in both my professional and personal life. <br/>
-                It's not just about coding, its about crafting a fulfilling and balanced lifestyle. <br/> <br/>
+                I'm currently pursuing Masters in Computer Applications from Amity University Online. <br />
+                Today my mission extends beyond merely securing a job. I aim not just to expand my knowledge
+                but to excel in both my professional and personal life. <br />
+                It's not just about coding, its about crafting a fulfilling and balanced lifestyle. <br /> <br />
 
-                On a side note, I'm a foodie, a musoc lover, a blogger, and a fitness enthusiast. 
+                On a side note, I'm a foodie, a musoc lover, a blogger, and a fitness enthusiast.
             </p>
         </div>
     )
--- src/components/DeveloperProfile.js
+++ src/components/DeveloperProfile.js
@@ -1,23 +1,56 @@
-const DeveloperProfile=()=>{
-    return(
-        <div style={{ color: 'white' }}>
-            <h2> As A Developer</h2>
-            <p>
-                I find immense satisfaction in crafting visually appealiung user interfaces. <br/>
-                My expertise spans a range of technologies including HTML, CSS, javascript, Reac.js and Redux.
-                I've honed my abilities through hands-on experiences and internships.<br/>
-                Crafting user friendly digital experiences, building reusable components and collaborating
-                with talented team have been my evenue of growth.
+import React, { useState } from 'react';
+import './DeveloperProfile.css';
+import { FaLongArrowAltLeft } from 'react-icons/fa'
+import { BsDashLg } from 'react-icons/bs'
 
-                In addition to my technical skills, i'vm proficient in modern development tools such as Git and
-                 well-versed popular integrated development Environments (IDEs) like visual studio code and Webstorm.<br/>
+
+const DeveloperProfile = () => {
+
+    const [showFullPara, setFullParaTongle] = useState(false)
+    const paragraphContent = showFullPara ? (
+        <>
+            <p className="full-paragraph-layout">
+                I find immense satisfaction in crafting visually appealing user interfaces. <br />
+                My expertise spans a range of technologies including HTML, CSS, JavaScript, React.js, and Redux.
+                I've honed my abilities through hands-on experiences and internships.<br />
+                Crafting user-friendly digital experiences, building reusable components, and collaborating
+                with talented teams have been my avenues of growth.
+
+                In addition to my technical skills, I'm proficient in modern development tools such as Git and
+                well-versed in popular Integrated Development Environments (IDEs) like Visual Studio Code and WebStorm.<br />
                 This efficiency allows me to maximize my productivity, ensuring top-notch results.
             </p>
-            <div>
-                CAPABLE MINDS OFTEN REMAIN UNSEEN TREASURES, <br/>
-                WAITING FOR THE CHANCE TO SHINE
+
+            <div className="caption">
+                <BsDashLg /> "CAPABLE MINDS OFTEN REMAIN UNSEEN TREASURES, <br />
+                WAITING FOR THE CHANCE TO SHINE"
             </div>
-        </div>
+        </>
+    ) : (
+        <>
+            <p>
+                I find immense satisfaction in crafting visually appealing user interfaces.My expertise
+                spans a range of technologies including HTML, CSS, JavaScript, React.js, and Redux.
+                I've honed my abilities through hands-on experiences and internships.<br />
+                Crafting user-friendly digital experiences, building reusable components, and
+                collaborating with talented teams have been my avenues of growth.
+            </p>
+            <div onClick={() => setFullParaTongle(true)}>
+                Learn More
+            </div>
+        </>
+    );
+
+    return (
+        <>
+            <div className={`parent ${showFullPara ? "column-layout" : ""}`}>
+                {showFullPara ?
+                    <div onClick={() => setFullParaTongle(false)}><FaLongArrowAltLeft className="bold-icon" /></div>
+                    : <h3> <BsDashLg /> Profession</h3>}
+                <h2> As A Developer</h2>
+                <p>{paragraphContent}</p>
+            </div>
+        </>
     )
 }
 export default DeveloperProfile;
